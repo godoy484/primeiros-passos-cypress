feff803caf42d5f95dcee9fa4380327f1c4846bb
Guilherme de Godoy <guilherme_gdy@hotmail.com>
2025-03-25 00:02:34 -0300

    modify cypress config

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -15,12 +15,11 @@ describe('Orange HRM Tests', () => {
     messageForgotPassword: '.orangehrm-forgot-password-title',
     messageTextSpan: '.oxd-text--span',
     buttonCancel: "[type='button']",
-    urlToVisit: 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login', 
     dashboardGrid: ".orangehrm-dashboard-grid"
   }
 
     it('1 - Login - Success', () => {
-    cy.visit(selectorsList.urlToVisit).wait(1000)
+    cy.visit('/auth/login').wait(1000)
     cy.get(selectorsList.usernameField).type(userData.userSucess.usarname)
     cy.get(selectorsList.passwordField).type(userData.userSucess.password)
     cy.get(selectorsList.loginButton).click()
@@ -28,38 +27,38 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorsList.dashboardGrid)
   })
   it('2 - Login - Fail - Username', () => {
-    cy.visit(selectorsList.urlToVisit).wait(1000)
+    cy.visit('/auth/login').wait(1000)
     cy.get(selectorsList.usernameField).type(userData.userFail.password)
     cy.get(selectorsList.passwordField).type(userData.userSucess.password)
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.wrongCredentialAlert)
   })
   it('3 - Login - Fail - Password', () => {
-    cy.visit(selectorsList.urlToVisit).wait(1000)
+    cy.visit('/auth/login').wait(1000)
     cy.get(selectorsList.usernameField).type(userData.userSucess.usarname)
     cy.get(selectorsList.passwordField).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.wrongCredentialAlert)
   })
   it('4 - Login - Fail - No Password', () => {
-    cy.visit(selectorsList.urlToVisit).wait(1000)
+    cy.visit('/auth/login').wait(1000)
     cy.get(selectorsList.usernameField).type(userData.userSucess.usarname)    
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.inputFielErroMenssage)
   })
   it('5 - Login - Fail - No Username', () => {
-    cy.visit(selectorsList.urlToVisit) .wait(1000)   
+    cy.visit('/auth/login') .wait(1000)   
     cy.get(selectorsList.passwordField).type(userData.userSucess.password)
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.inputGroupMessage)
   })
   it('6 - Login - Fail - No Usarname/Password', () => {
-    cy.visit(selectorsList.urlToVisit).wait(1000)
+    cy.visit('/auth/login').wait(1000)
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.inputGroupMessage)
   })
   it('7 - Login - Fail - Forgot your Password', () => {
-    cy.visit(selectorsList.urlToVisit).wait(1000)
+    cy.visit('/auth/login').wait(1000)
     cy.get(selectorsList.messageForgotHeader).click()
     cy.get(selectorsList.resetPassword)    
     cy.get(selectorsList.usernameField).type(userData.userSucess.usarname)
@@ -67,14 +66,14 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorsList.messageForgotPassword)
   })
   it('8 - Login - Fail - Forgot your Password - No Usarname', () => {
-    cy.visit(selectorsList.urlToVisit).wait(1000)
+    cy.visit('/auth/login').wait(1000)
     cy.get(selectorsList.messageForgotHeader).click()
     cy.get(selectorsList.resetPassword)        
     cy.get(selectorsList.loginButton).click() 
     cy.get(selectorsList.messageTextSpan)
   })
   it('9 - Login - Fail - Forgot your Password - Return Home', () => {
-    cy.visit(selectorsList.urlToVisit).wait(1000)
+    cy.visit('/auth/login').wait(1000)
     cy.get(selectorsList.messageForgotHeader).click()
     cy.get(selectorsList.resetPassword)        
     cy.get(selectorsList.loginButton).click() 
