a86127b4b61b74258de78f889421587969234b74
Guilherme de Godoy <guilherme_gdy@hotmail.com>
2025-03-24 22:43:27 -0300

    Added selectors list for login spec

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,65 +1,82 @@
 describe('Orange HRM Tests', () => {
+
+  const selectorsList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    wrongCredentialAlert: "[role='alert']",
+    inputFielErroMenssage: '.oxd-input-field-error-message',
+    inputGroupMessage: '.oxd-input-group__message',
+    messageForgotHeader: '.orangehrm-login-forgot-header',
+    resetPassword: '.oxd-text--h6',
+    messageForgotPassword: '.orangehrm-forgot-password-title',
+    messageTextSpan: '.oxd-text--span',
+    buttonCancel: "[type='button']",
+    urlToVisit: 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
+  }
+
   it('1 - Login - Success', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login').wait(1000)
-    cy.get("[name='username']").type('Admin')
-    cy.get("[name='password']").type('admin123')
-    cy.get("[type='submit']").click()
+    cy.visit(selectorsList.urlToVisit).wait(1000)
+    cy.get(selectorsList.usernameField).type('Admin')
+    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
+    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
   })
   it('2 - Login - Fail - Username', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login').wait(1000)
-    cy.get("[name='username']").type('teste')
-    cy.get("[name='password']").type('admin123')
-    cy.get("[type='submit']").click()
-    cy.get("[role='alert']")
+    cy.visit(selectorsList.urlToVisit).wait(1000)
+    cy.get(selectorsList.usernameField).type('teste')
+    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.wrongCredentialAlert)
   })
   it('3 - Login - Fail - Password', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login').wait(1000)
-    cy.get("[name='username']").type('Admin')
-    cy.get("[name='password']").type('teste')
-    cy.get("[type='submit']").click()
-    cy.get("[role='alert']")
+    cy.visit(selectorsList.urlToVisit).wait(1000)
+    cy.get(selectorsList.usernameField).type('Admin')
+    cy.get(selectorsList.passwordField).type('teste')
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.wrongCredentialAlert)
   })
   it('4 - Login - Fail - No Password', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login').wait(1000)
-    cy.get("[name='username']").type('Admin')    
-    cy.get("[type='submit']").click()
-    cy.get('.oxd-input-field-error-message')
+    cy.visit(selectorsList.urlToVisit).wait(1000)
+    cy.get(selectorsList.usernameField).type('Admin')    
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.inputFielErroMenssage)
   })
   it('5 - Login - Fail - No Username', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login') .wait(1000)   
-    cy.get("[name='password']").type('admin123')
-    cy.get("[type='submit']").click()
-    cy.get('.oxd-input-group__message')
+    cy.visit(selectorsList.urlToVisit) .wait(1000)   
+    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.inputGroupMessage)
   })
   it('6 - Login - Fail - No Usarname/Password', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login').wait(1000)
-    cy.get("[type='submit']").click()
-    cy.get('.oxd-input-group__message')
+    cy.visit(selectorsList.urlToVisit).wait(1000)
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.inputGroupMessage)
   })
   it('7 - Login - Fail - Forgot your Password', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login').wait(1000)
-    cy.get('.orangehrm-login-forgot-header').click()
-    cy.get('.oxd-text--h6')    
-    cy.get("[name='username']").type('Admin')
-    cy.get("[type='submit']").click() 
-    cy.get('.orangehrm-forgot-password-title')
+    cy.visit(selectorsList.urlToVisit).wait(1000)
+    cy.get(selectorsList.messageForgotHeader).click()
+    cy.get(selectorsList.resetPassword)    
+    cy.get(selectorsList.usernameField).type('Admin')
+    cy.get(selectorsList.loginButton).click() 
+    cy.get(selectorsList.messageForgotPassword)
   })
   it('8 - Login - Fail - Forgot your Password - No Usarname', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login').wait(1000)
-    cy.get('.orangehrm-login-forgot-header').click()
-    cy.get('.oxd-text--h6')        
-    cy.get("[type='submit']").click() 
-    cy.get('.oxd-text--span')
+    cy.visit(selectorsList.urlToVisit).wait(1000)
+    cy.get(selectorsList.messageForgotHeader).click()
+    cy.get(selectorsList.resetPassword)        
+    cy.get(selectorsList.loginButton).click() 
+    cy.get(selectorsList.messageTextSpan)
   })
   it('9 - Login - Fail - Forgot your Password - Return Home', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login').wait(1000)
-    cy.get('.orangehrm-login-forgot-header').click()
-    cy.get('.oxd-text--h6')        
-    cy.get("[type='submit']").click() 
-    cy.get('.oxd-text--span')
-    cy.get("[type='button']").click()
-    cy.get('.orangehrm-login-forgot-header')
+    cy.visit(selectorsList.urlToVisit).wait(1000)
+    cy.get(selectorsList.messageForgotHeader).click()
+    cy.get(selectorsList.resetPassword)        
+    cy.get(selectorsList.loginButton).click() 
+    cy.get(selectorsList.messageTextSpan)
+    cy.get(selectorsList.buttonCancel).click()
+    cy.get(selectorsList.messageForgotHeader)
   })
 })
